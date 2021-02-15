7a0311875aa94b9b72cfc05b9c82f4e0e9c563e7
Aleti-Prabhas <54319984+Aleti-Prabhas@users.noreply.github.com>
2021-02-15 11:24:17 +0530

    Update reverse_number.py
    the final executable program of reversing of both positive and negative numbers within the range of 32 bits

--- reverse_number.py
+++ reverse_number.py
@@ -1,10 +1,19 @@
-x=int(input())
-a = str(x)
+class Solution:
+    def reverse(self, x: int) -> int:
+        a = str(x)
 
-if 0 < x <= (1 << 31)-1:
-    print(int(a[::-1]))
+        if 0 < x <= (2**31)-1:
+            ans=int(a[::-1])
+            if(0<ans<=(2**31)-1):
+                return ans
+            else:
+                return 0
 
-elif (-1 << 31) <= x < 0:
-     print(-(int(a[:-len(a):-1])))
-else:
-    print(0)
+        elif (-2**31) <= x < 0:
+            ans=-(int(a[:-len(a):-1]))
+            if((-2**31) <= ans < 0):
+                return ans
+            else:
+                return 0
+        else:
+            return 0
